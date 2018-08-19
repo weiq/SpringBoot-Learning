25227f69df76bb42817c9258187701b3e198d67a
魏小雨 <6682220aabc@163.com>
2018-08-19 21:57:57 +0800

    update

--- lesson2/src/main/java/com/lanif/lesson2/Lesson2Application.java
+++ lesson2/src/main/java/com/lanif/lesson2/Lesson2Application.java
@@ -1,9 +1,11 @@
 package com.lanif.lesson2;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.lanif.lesson2.dao")
 public class Lesson2Application {
 
     public static void main(String[] args) {
