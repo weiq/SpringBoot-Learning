4bb2beab4c66fbd86ba4f2b0b5b1f9c804be1702
魏小雨 <6682220aabc@163.com>
2018-08-19 10:59:46 +0800

    update

---
+++ lesson1/src/main/java/com/weiq/lesson1/Lesson1Application.java
@@ -0,0 +1,12 @@
+package com.weiq.lesson1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lesson1Application.class, args);
+    }
+}
